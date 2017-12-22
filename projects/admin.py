78ef8f190d4b1e7baf32ceb503586b3c41916016
pw94 <pawelmwinnicki@gmail.com>
2017-12-22 23:27:26 +0100

    Register Category for admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
-# Register your models here.
+from projects.models import Category
+
+admin.site.register(Category, MPTTModelAdmin)
